e680593a48bf24a33d6f3cfd438de3efe02b974e
Agostina Celina Venezia Napolillo <agostina.cv@gmail.com>
2023-10-23 15:50:31 -0300

    Clase 01 - Conversor de monedas

---
+++ Clase01-Conversor/conversor-ethereum/script.js
@@ -0,0 +1,6 @@
+var nombreUsuario = "Jeanmarie";
+var valorEnEthereum = 3;
+var cotizacionEnEthereum = 8109.71;
+var valorEnPeso = (valorEnEthereum * cotizacionEnEthereum).toFixed(2);
+// valorEnPeso = valorEnPeso.toFixed(2);
+alert('Hola ' + nombreUsuario + ', el valor en reales es de R$ '+ valorEnPeso);
